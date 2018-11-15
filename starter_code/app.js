bb2c1b36736e1b54c233ec44b94261d0150bc8b0
jrecio <recio.juan@gmail.com>
2018-11-15 15:47:07 +0100

    Iteration 1

---
+++ starter_code/app.js
@@ -0,0 +1,18 @@
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// Remember to paste your credentials here
+var clientId = '3369f0eac8774e56b4f9a30ae269a041',
+    clientSecret = '3c86188f0b504ad391456338637d804b';
+
+var spotifyApi = new SpotifyWebApi({
+  clientId : clientId,
+  clientSecret : clientSecret
+});
+
+// Retrieve an access token.
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong when retrieving an access token', err);
+});
