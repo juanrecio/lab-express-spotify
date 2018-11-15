9fcfa2a5a26fdbcec0e4329b9f70fec2276c7d23
jrecio <recio.juan@gmail.com>
2018-11-15 16:17:20 +0100

    Iteration 3.1

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,6 +3,11 @@ var SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const path= require('path');
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname,'public')));
 
 // Remember to paste your credentials here
 var clientId = '3369f0eac8774e56b4f9a30ae269a041',
@@ -20,3 +25,10 @@ spotifyApi.clientCredentialsGrant()
   }, function(err) {
     console.log('Something went wrong when retrieving an access token', err);
 });
+
+app.get('/',(req,res)=>{
+  res.render('home');
+})
+
+
+app.listen(3000);
