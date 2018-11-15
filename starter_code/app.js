10308d3f95817892e230b30fb0d91f32ab02d332
jrecio <recio.juan@gmail.com>
2018-11-15 18:57:06 +0100

    Iteration 3.2 sin terminar

--- starter_code/app.js
+++ starter_code/app.js
@@ -3,32 +3,63 @@ var SpotifyWebApi = require('spotify-web-api-node');
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
-const path= require('path');
+const path = require('path');
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+hbs.registerPartials(__dirname + '/views/partials')
 
 // Remember to paste your credentials here
 var clientId = '3369f0eac8774e56b4f9a30ae269a041',
-    clientSecret = '3c86188f0b504ad391456338637d804b';
+  clientSecret = '3c86188f0b504ad391456338637d804b';
 
 var spotifyApi = new SpotifyWebApi({
-  clientId : clientId,
-  clientSecret : clientSecret
+  clientId: clientId,
+  clientSecret: clientSecret
 });
 
 // Retrieve an access token.
 spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
+  .then(function (data) {
     spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
+  }, function (err) {
     console.log('Something went wrong when retrieving an access token', err);
+  });
+
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
-app.get('/',(req,res)=>{
-  res.render('home');
-})
+
+
+app.get('/artists', (req, res) => {
+  // console.log(req.body);
+  spotifyApi.searchArtists(req.query.artist)
+    .then(data => {
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.render('artists', {data});
+    })
+    .catch(err => {
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    })
+  
+});
+
+app.get('/art', (req, res) => {
+  // console.log(req.body);
+  spotifyApi.searchArtists("a")
+    .then(data => {
+      // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+      res.json(data);
+    })
+    .catch(err => {
+      // ----> 'HERE WE CAPTURE THE ERROR'
+    })
+  
+});
+
 
 
 app.listen(3000);
